9a508d021cb6aa6518aed1058a909b5e41d952bf
revanangrh <revan.anugerah_ti24@nusaputra.ac.id>
2024-10-19 20:27:56 +0700

    Create Deret1_Revan.java

---
+++ Deret1_Revan.java
@@ -0,0 +1,13 @@
+public class Deret1_Revan {
+    public static void main(String[] args) {
+       
+        for (int i = 0; i < 3; i++) {
+            
+            for (int j = 1; j <= 6; j++) {
+                System.out.print(j + " ");
+            }
+           
+            System.out.println();
+        }
+    }
+}
